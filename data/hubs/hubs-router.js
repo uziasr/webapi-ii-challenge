354a92daa14f274831815fb5307b2259aa2f605d
Uzias Rivera <uziasrivera@gmail.com>
2019-10-29 15:34:57 -0700

    completed more functionality

--- data/hubs/hubs-router.js
+++ data/hubs/hubs-router.js
@@ -10,7 +10,7 @@ router.get('/', (req, res)=>{
         res.status(201).json(post)
     })
     .catch(err =>{
-        res.status(500).json({error: 'could not retrieve any posts from the database'})
+        res.status(500).json({ error: "The posts information could not be retrieved." })
     })
 })
 
@@ -24,4 +24,48 @@ router.post('/', (req, res)=>{
     .catch(error =>{
         res.status(500).json({ error: "There was an error while saving the post to the database" })
     }): res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+})
+
+router.post('/:id', (req, res)=>{
+    //needs 400
+    const id = req.params.id
+    console.log(req.body,id)
+    "text" in req.body?
+    db.insertComment({...req.body,post_id:id})
+    .then((id)=>{
+        console.log(id)
+        res.status(201).json(req.body)})
+    .catch((err)=>{
+        console.log(err)
+        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+    }): res.status(400).json({ errorMessage: "Please provide text for the comment." })
+})
+
+router.get('/:id', (req, res)=>{
+    const id = req.params.id
+    db.findById(id)
+    .then(post=>{
+        post.length?
+        res.status(200).json(post): res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(err=>{
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
+
+router.get('/:id/comments', (req, res)=>{
+    const id = req.params.id
+    db.findPostComments(id)
+    .then((posts)=>{res.status(200).json(posts)})
+    .catch(err=>{res.status(500).json({ error: "The comments information could not be retrieved." })})
+})
+
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id
+    db.remove(id)
+    .then(()=>{
+        res.status(200).json({success: `post with id ${id} was removed`})
+    })
+    .catch(err=> res.status(500).json({ error: "The post could not be removed" }))
+
 })
